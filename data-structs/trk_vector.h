fe83cb4a251789d3ae220467850ef39466b3d2c0
ashenBlade <htcdnsnnkom2002@gmail.com>
2020-09-13 19:34:19 +0300

    Added element managing methods

---
+++ data-structs/trk_vector.h
@@ -0,0 +1,22 @@
+#ifndef TRK_LIBRARY_H
+#define TRK_LIBRARY_H
+
+template<class T>
+class TRK_Vector {
+private:
+    T *data;
+    long int size;
+public:
+    explicit TRK_Vector();
+    explicit TRK_Vector(int sizeOfArray);
+    ~TRK_Vector();
+
+    void Resize(int newSize);
+    long int getSize();
+    T *operator[](long int index);
+    T *at(long int index);
+    T *front();
+    T *back();
+};
+
+#endif //TRK_LIBRARY_H
